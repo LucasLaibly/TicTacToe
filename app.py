dc80d913fafc8ab1a47eb1bf813ec32d613172b5
LucasLaibly <original7245@gmail.com>
2020-11-12 20:38:30 -0600

    win logic, reset endpoint

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import session
 from flask import render_template
+from flask import redirect
+from flask import url_for
 from flask_session import Session
 from datetime import datetime
 import re
@@ -23,36 +25,59 @@ def base():
         board=session["board"]
     )
 
+# Reset the board
+@app.route("/reset")
+def reset():
+    session["turn"]   = "X"
+    session["winner"] = False
+    session["board"]  = [[None, None, None], [None, None, None],[None, None, None]]
+
+    return redirect(url_for("base"))
+
 # Play an action
 @app.route("/action/<int:row>/<int:col>")
 def action(row, col):
+    player = session["turn"]
     if session["board"][row][col] is None:
         session["board"][row][col] = session["turn"]
+    
+    result = is_winner(session["board"], session["turn"])
 
     if session["turn"] == "X":
             session["turn"] = "O"
     else:
         session["turn"] = "X"
 
-    return render_template(
+    if result == False:
+        return render_template(
         "board.html",
         board=session["board"]
     )
-
-# DFS Cross
-def dfs(board, row, col):
-    stack = [] # probaly will continually reset the stack
-    if board[row][col] == "X":
-        stack.append((row, col))
     else:
-        return True if len(stack) == 3 else False
-    
-    if row+1 < len(board):
-        dfs(board, row+1, col)
-    if row-1 >= 0:
-        dfs(board, row-1, col)
-    if col+1 < len(board[0]):
-        dfs(board, row, col+1)
-    if col-1 >= 0:
-        dfs(board, row, col-1)
-    
+        return render_template(
+        "winner.html",
+        board=session["board"],
+        player=player
+    )
+
+def is_winner(board, turn):
+    n = len(board)
+    for indexes in win_cons(n):
+        if all(board[r][c] == turn for r, c in indexes):
+            return True
+    return False
+
+def win_cons(n):
+    # Rows
+    for r in range(n):
+        yield [(r, c) for c in range(n)]
+
+    # Columns
+    for c in range(n):
+        yield [(r, c) for r in range(n)]
+
+    # Diagonal top left to bottom right
+    yield [(i, i) for i in range(n)]
+
+    # Diagonal top right to bottom left
+    yield [(i, n - 1 - i) for i in range(n)]
